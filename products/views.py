005f6bce120aeae764df831a347797b1240bad34
VTwin90 <vanja.torp1@gmail.com>
2022-02-22 19:58:20 +0000

    fixing the image field in product edit/update some minor fixes

--- products/views.py
+++ products/views.py
@@ -76,7 +76,7 @@ def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home')
+        return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -102,7 +102,7 @@ def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home')
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -131,7 +131,7 @@ def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home')
+        return redirect(reverse('home'))
         
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
